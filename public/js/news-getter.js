b15ecd991234334dde2ec33ea1e6c150b1f879b8
irimoti <52734487+r119306@users.noreply.github.com>
2022-01-19 23:25:34 +0900

    fix-og

--- public/js/news-getter.js
+++ public/js/news-getter.js
@@ -1,38 +1,30 @@
-const url = [
-    "https://miri-home-page.vercel.app/article/202201181",
-    "https://miri-home-page.vercel.app/article/202201181",
-    "https://miri-home-page.vercel.app/article/202201181",
-    "https://miri-home-page.vercel.app/article/202201181",
-    "https://miri-home-page.vercel.app/article/202201181"
+const date = [
+    "202201181",
+    "202201181",
+    "202201181",
+    "202201181",
+    "202201181"
 ];
 
-// const url = [
-//     "https://isirmt.d3bu.net/article/",
-//     "https://isirmt.d3bu.net/article/readme/",
-//     "https://isirmt.d3bu.net/article/",
-//     "https://isirmt.d3bu.net/",
-//     "https://isirmt.d3bu.net/article/versions-file/"
-// ];
-
-url.forEach(function (elem, i) {
-    fetch(elem).then(res => res.text()).then(text => {
+date.forEach(async (elem, i) => {
+    fetch("https://miri-home-page.vercel.app/article/" + elem).then(res => res.text()).then(text => {
         const el = new DOMParser().parseFromString(text, "text/html")
         const head_els = (el.head.children)
         Array.from(head_els).map(v => {
             const prop = v.getAttribute('property');
             if (!prop) return;
 
-            if (prop == 'og:title'){
-                $(".nt"+[i+1]).html(v.getAttribute("content"));
+            if (prop == 'og:title') {
+                $(".nt" + [i + 1]).html(v.getAttribute("content"));
             }
-            if (prop == 'og:description'){
-                $(".nd"+[i+1]).html(v.getAttribute("content"));
+            if (prop == 'og:description') {
+                $(".nd" + [i + 1]).html(v.getAttribute("content"));
             }
-            if (prop == 'og:url'){
-                $(".nu"+[i+1]).attr('href', v.getAttribute("content"));
+            if (prop == 'og:url') {
+                $(".nu" + [i + 1]).attr('href', v.getAttribute("content"));
             }
-            if (prop == 'og:image'){
-                $(".ni"+[i+1]).attr('src', v.getAttribute("content"));
+            if (prop == 'og:image') {
+                $(".ni" + [i + 1]).attr('src', v.getAttribute("content"));
             }
         })
     })
