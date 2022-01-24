6fcec4b69ffdbbfac710f862aad997a5e409b6dc
irimoti <52734487+r119306@users.noreply.github.com>
2022-01-25 01:30:07 +0900

    new file tree

---
+++ api/js/news-getter.js
@@ -0,0 +1,31 @@
+const date = [
+    "202201181",
+    "202201181",
+    "202201181",
+    "202201181",
+    "202201181"
+];
+
+date.forEach(async (elem, i) => {
+    fetch("https://miri-home-page.vercel.app/article/" + elem).then(res => res.text()).then(text => {
+        const el = new DOMParser().parseFromString(text, "text/html")
+        const head_els = (el.head.children)
+        Array.from(head_els).map(v => {
+            const prop = v.getAttribute('property');
+            if (!prop) return;
+
+            if (prop == 'og:title') {
+                $(".nt" + [i + 1]).html(v.getAttribute("content"));
+            }
+            if (prop == 'og:description') {
+                $(".nd" + [i + 1]).html(v.getAttribute("content"));
+            }
+            if (prop == 'og:url') {
+                $(".nu" + [i + 1]).attr('href', v.getAttribute("content"));
+            }
+            if (prop == 'og:image') {
+                $(".ni" + [i + 1]).attr('src', v.getAttribute("content"));
+            }
+        })
+    })
+})
---
+++ api/php/latest-news.php
@@ -0,0 +1,38 @@
+<?php
+
+function console_log($data)
+{
+    echo '<script>';
+    echo 'console.log(' . json_encode($data) . ')';
+    echo '</script>';
+}
+
+$path = dirname(__FILE__) . '/';
+
+$dirs = scandir($path);
+
+$excludes = array(
+    '.',
+    // '..',
+    // 'css',
+    // 'test',
+    // 'test2',
+);
+
+// echo '<ul>', "\n";
+foreach ($dirs as $dir) {
+
+    if (in_array($dir, $excludes)) {
+        continue;
+    }
+
+    if ((is_dir($dir) === true)) {
+        // echo '<li>';
+        // echo '<a href="./' . $dir . '">';
+        // echo ($dir);
+        // echo '</a></li>' . "\n";
+        console_log($dir);
+    }
+}
+// echo '</ul>', "\n";
+?>
